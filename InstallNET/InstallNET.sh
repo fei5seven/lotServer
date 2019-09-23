fc9b49bf2c064cb4430b1d833a683223bbc1165e
fei5seven <usb@live.hk>
2019-09-23 12:36:47 +0800

    重装系统

--- InstallNET/InstallNET.sh
+++ InstallNET/InstallNET.sh
@@ -327,7 +327,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://moeclub.org/get/wget_udeb_amd64';
+  export SSL_SUPPORT='https://raw.githubusercontent.com/fei5seven/lotServer/master/InstallNET/wget_udeb_amd64';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
