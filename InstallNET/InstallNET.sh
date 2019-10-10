a83ae28a2cffcff67d3b1cc86c2eb3e3d91d9103
fei5seven <usb@live.hk>
2019-10-11 02:26:28 +0800

    Update InstallNET.sh

--- InstallNET/InstallNET.sh
+++ InstallNET/InstallNET.sh
@@ -345,7 +345,7 @@ fi
 
 [ -n "$ipAddr" ] && [ -n "$ipMask" ] && [ -n "$ipGate" ] && setNet='1';
 [[ -n "$tmpWORD" ]] && myPASSWORD="$(openssl passwd -1 "$tmpWORD")";
-[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$4BJZaD0A$y1QykUnJ6mXprENfwpseH0';
+[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$fcyWSnlM$.bTFB6O67KZKRqJAPqpeH1';
 
 if [[ -n "$interface" ]]; then
   IFETH="$interface"
