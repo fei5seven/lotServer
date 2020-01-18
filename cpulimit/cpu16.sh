b0665a84a69f8bf9136b28159d390cc64def832c
fei5seven <usb@live.hk>
2020-01-19 01:17:38 +0800

    添加cpulimit脚本

---
+++ cpulimit/cpu16.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 50
---
+++ cpulimit/cpu18.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_18.04_4.15.0-30-generic] -l 50
