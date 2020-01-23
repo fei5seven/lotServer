5acf3121a47dfeef525d28fcc1e1bf7d37a69cdb
fei5seven <usb@live.hk>
2020-01-23 19:10:55 +0800

    修改配置

--- cpulimit/cpu16.sh
+++ cpulimit/cpu16.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+sleep 15
  cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 50
--- cpulimit/cpu18.sh
+++ cpulimit/cpu18.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+sleep 15
  cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_18.04_4.15.0-30-generic] -l 50
