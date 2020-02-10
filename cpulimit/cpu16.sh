64afb015d97d6d6f1a562ec7f8bcbb85c5470be0
fei5seven <usb@live.hk>
2020-02-10 18:33:40 +0800

    update

--- cpulimit/cpu16.sh
+++ cpulimit/cpu16.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sleep 15
- cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 50
+{
+sysctl -w vm.panic_on_oom=1
+sysctl -p
+nohup cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 70 m>> /dev/null 2>&1 &
+}
--- cpulimit/cpu18.sh
+++ cpulimit/cpu18.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sleep 15
- cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_18.04_4.15.0-30-generic] -l 50
+{
+sysctl -w vm.panic_on_oom=1
+sysctl -p
+nohup cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_18.04_4.15.0-30-generic] -l 70 m>> /dev/null 2>&1 &
+}
