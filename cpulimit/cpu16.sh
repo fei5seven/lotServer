d4e609f09d22e60b6c9237a272dff12495968d34
fei5seven <usb@live.hk>
2020-02-12 02:09:49 +0800

    瞎改

--- cpulimit/cpu16.sh
+++ cpulimit/cpu16.sh
@@ -2,5 +2,5 @@
 {
 sysctl -w vm.panic_on_oom=1
 sysctl -p
-nohup cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 70 m>> /dev/null 2>&1 &
+cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 70
 }
