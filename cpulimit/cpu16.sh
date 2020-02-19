22b6e36b14dc65b117cfa862eed381a1fa216591
fei5seven <usb@live.hk>
2020-02-20 01:15:35 +0800

    实验性测试...

--- cpulimit/cpu16.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-{
-sysctl -w vm.min_free_kbytes=30000
-sysctl -w vm.panic_on_oom=1
-sysctl -p
-cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_16.04_4.8.0-36-generic] -l 70
-}
--- cpulimit/cpu18.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-{
-sysctl -w vm.panic_on_oom=1
-sysctl -p
-nohup cpulimit -e /appex/bin/acce-3.11.36.2-[Ubuntu_18.04_4.15.0-30-generic] -l 70 m>> /dev/null 2>&1 &
-}
