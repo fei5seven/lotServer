ac12c08f2dcc6f0c8f1c0ed02401601e1c25530c
irsb <usb@live.hk>
2023-09-17 15:14:48 +0800

    Update lotServerInstall.sh

--- lotServerInstall.sh
+++ lotServerInstall.sh
@@ -90,7 +90,7 @@ function Install()
   tar -xvf "/tmp/lotServer.tar" -C /tmp
   acce_ver=$(acce_check ${KNV})
   # 如果有自己搭建的或者api失效，这里修改成你自己的api即可
-  wget --no-check-certificate -qO "${AcceTmp}/etc/apx.lic" "https://apx.irsb.xyz/keygen.php?ver=${acce_ver}&mac=${Mac}"
+  wget --no-check-certificate -qO "${AcceTmp}/etc/apx.lic" "https://speed.irsb.xyz/keygen.php?ver=${acce_ver}&mac=${Mac}"
   [ "$(du -b ${AcceTmp}/etc/apx.lic |cut -f1)" -lt '152' ] && Uninstall "Error! I can not generate the Lic for you, Please try again later. "
   echo "Lic generate success! "
   sed -i "s/^accif\=.*/accif\=\"$Eth\"/" "${AcceTmp}/etc/config"
